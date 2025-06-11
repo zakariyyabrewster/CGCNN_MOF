e3d91cc302ece55361e2ff1d6d2f51ecb02a866a
zakariyyabrewster <zakariyya@sprung-brett.com>
2025-06-11 19:33:36 -0400

    separate scripts into extraction and training for debugging

---
+++ scripts/train_cgcnn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR" # root/scripts
+cd .. # root
+
+# TARGETS=("Di" "CH4_HP" "CO2_LP" "logKH_CO2")
+TARGETS=$@
+
+# Loop over target properties
+for TARGET in "${TARGETS[@]}"; do
+    echo "=============================="
+    echo "Training on target: $TARGET"
+    echo "=============================="
+
+    # Run training script with the current target_property
+    python finetune_cgcnn.py --config config_ft_cgcnn.yaml --target_prop $TARGET
+
+    echo "Finished training on target: $TARGET"
+    echo ""
+done

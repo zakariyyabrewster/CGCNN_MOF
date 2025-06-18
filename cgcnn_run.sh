64d7c37ff8798061964af0bfea331a06fc50061a
zakariyyabrewster <zakariyya@sprung-brett.com>
2025-06-18 15:35:21 -0400

    add dependencies

--- cgcnn_run.sh
+++ cgcnn_run.sh
@@ -10,11 +10,15 @@
 
 cd $SLURM_SUBMIT_DIR
 
+module load StdEnv/2023
 module load python/3.11.5
-module load cuda/11.8.0
-module load cudnn/8.7.0.84
+module load cudacore/.12.6.2
+module load cuda/12.6.0
+module load cudnn/9.5.1.17
+module load yaml/2.3.10
 
-source $REPO_DIR/myenv/bin/activate
+
+source myenv/bin/activate
 
 bash extract_db.sh # extract tar file of CoRE2019 
 bash train_cgcnn.sh Di # train model on Di

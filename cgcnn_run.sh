651a33bc404f35760c6d3ec32cbdc57ee38441d3
zakariyyabrewster <zakariyya@sprung-brett.com>
2025-06-23 16:39:50 -0400

    config changes

--- cgcnn_run.sh
+++ cgcnn_run.sh
@@ -4,13 +4,15 @@
 #SBATCH --job-name=cgcnn_run
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=8G
-#SBATCH --time=0-00:30
+#SBATCH --mem=32G
+#SBATCH --time=0-02:00
 #SBATCH --gres=gpu:1
 
 cd $SLURM_SUBMIT_DIR
 
 module load StdEnv/2023
+module load gcc/12.3
+module load r-bundle-bioconductor/3.20
 module load python/3.11.5
 module load cudacore/.12.6.2
 module load cuda/12.6.0
@@ -20,5 +22,5 @@ module load yaml/2.3.10
 
 source myenv/bin/activate
 
-bash extract_db.sh # extract tar file of CoRE2019 
-bash train_cgcnn.sh Di # train model on Di
+bash extract_db.sh # extract tar file of CoRE2019, only have to do once in env
+bash train_cgcnn.sh Di # train model on Di, CH4_HP, CO2_LP, logKH_CO2

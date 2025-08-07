bdbaf81ebcfd0899134b38910a97f532b9a20ef0
zakariyyabrewster <zakariyya@sprung-brett.com>
2025-08-07 12:10:26 -0400

    renaming overhaul

--- kcv_cgcnn.sh
+++ kcv_cgcnn.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-# TARGETS=("Di" "CH4_HP" "CO2_LP" "logKH_CO2")
+#SBATCH --account=def-moosavi5
+#SBATCH --job-name=kcv_cgcnn_run
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40G
+#SBATCH --time=0-10:00
+#SBATCH --gres=gpu:1
+
+cd $SLURM_SUBMIT_DIR
+
+module load StdEnv/2023
+module load gcc/12.3
+module load r-bundle-bioconductor/3.20
+module load python/3.11.5
+module load cudacore/.12.6.2
+module load cuda/12.6
+module load cudnn/9.5.1.17
+
+
+source myenv/bin/activate
+
+pip install -r requirements.txt
+
+# bash extract_db.sh
+
 TARGETS=("$@") # Accept target properties as command line arguments
 
 # Loop over target properties
